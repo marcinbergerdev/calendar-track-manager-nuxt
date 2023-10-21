94e3c1a7ec32a81a01d54277c454db8ecc7422b2
marcinbergerdev <marcin.berger95@op.pl>
2023-10-21 14:58:38 +0200

    feat: add user log status and auth button visibility

--- components/nav/Auth.vue
+++ components/nav/Auth.vue
@@ -1,17 +1,18 @@
 <template>
-  <li class="auth-item">
+ 
+  <li class="auth-item" v-if="!userStatus">
     <NuxtLink to="/" class="nav-animation" @click="logIn">log in</NuxtLink>
   </li>
-  <li class="auth-item">
+  <li class="auth-item" v-if="!userStatus">
     <NuxtLink to="/" class="nav-animation" @click="signUp">sign up</NuxtLink>
   </li>
-  <li class="auth-item">
+  <li class="auth-item" v-if="!!userStatus">
     <button class="nav-animation" @click="signOut">sign out</button>
   </li>
 </template>
 
 <script setup lang="ts">
-const userStatus = useUserLogStatus();
+const userStatus = useCookie('userUidStatus');
 
 const email = ref("test@test.com");
 const password = ref("test123456");
--- composables/useFirebaseAuth.ts
+++ composables/useFirebaseAuth.ts
@@ -27,7 +27,10 @@ export const signInUser = async (email: string, password: string) => {
    await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
          const user = userCredential.user;
+         const userUidStatus = useCookie('userUidStatus');
+         userUidStatus.value = user.uid
          console.log(user);
+
       })
       .catch((error) => {
          const errorCode = error.code;
@@ -52,9 +55,10 @@ export const userUpdateStatus = () => {
 };
 
 export const signOutUser = () => {
-   const userStatus = useUserLogStatus();
    const auth = getAuth();
+   const userUidStatus = useCookie('userUidStatus');
    auth.signOut();
-   userStatus.value = false;
+   userUidStatus.value = "false"
+
    console.log("log out");
 };
